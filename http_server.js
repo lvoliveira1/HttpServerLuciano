c5a92c1ee7695afc20eb3d306cc3ea557ff04fa6
lvoliveira1 <lvoliveira1@gmail.com>
2022-06-05 22:40:13 -0500

    trying to fix everything

--- http_server.js
+++ http_server.js
@@ -5,6 +5,7 @@ const fs      = require('lowdb/adapters/FileSync');
 const adapter = new fs('db.json');
 const db      = low(adapter);
 const cors    = require('cors');
+const port    = process.env.PORT;
 
 // allow cross-origin resource sharing (CORS)
 app.use(cors());
@@ -48,6 +49,6 @@ app.post('/add', function(req, res){
 
 // start server
 // -----------------------
-app.listen(process.env.PORT || 3000, function(){
+app.listen(port || 3000, function(){
     console.log('Running on port from heroku!')
 })
